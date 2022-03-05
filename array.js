9d3f861d9f6f34de716a05a98cb3854742852cc6
ngengngeng <oscarfabiorico76@gmail.com>
2022-03-05 13:07:00 +0700

    jawabansoalarray

---
+++ array.js
@@ -0,0 +1,42 @@
+
+// soal 1a
+// var watches = ["Swiss Army","Daniel Wellington","Dublot"];
+//     console.log("Saya" + " " + "punya" + " " + watches[0] + "," + " " + watches[1] + "," + " " + "dan" + " " + watches[2] + ".")
+
+
+// soal 1b
+// var arloji = ["A","B","C","D","E"];
+//     console.log("Saya" + " " + "punya" + " " + arloji[0] + ", " + arloji[1] + ", " + arloji[2] + ", " + arloji[3] + ", " + "dan" + " " + arloji[4] + ".")
+
+// soal 2
+// let fruits = ["cherry", "blueberry", "avocado", "apple", "cranberry"];
+
+// var fruits = ["cherry", "blueberry", "avocado", "apple", "cranberry"];
+// fruits.sort();
+// // console.log(fruits)
+
+// for (var i; i < fruits.length; i ++){
+//     if(fruit[i].sort; 
+// }
+
+// soal 3
+// let kalimat = "belajar bersama dunia coding yuk"
+
+// var kalimat = "belajar bersama dunia coding yuk";
+// var tempStr = "";
+// var tempArray = []
+
+// for(var i = 0; i < kalimat.length; i++){
+//       if(kalimat[i] !== " "){
+//         tempStr += kalimat[i]
+//     }
+//     else{
+//         tempArray.push(tempStr)
+//         tempStr = ""
+//     }
+//     if(i === kalimat.length - 1){
+//         tempArray.push(tempStr)
+//         tempStr = ""
+//     }
+// }
+// console.log(tempArray)
