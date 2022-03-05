cba7b2ba70bcfc999fbe3ead9151efaef351b0d1
ngengngeng <oscarfabiorico76@gmail.com>
2022-03-05 10:33:49 +0700

    jawabansoalloping

---
+++ looping.js
@@ -0,0 +1,38 @@
+//soal 1
+var number = 10;
+var faktor = 1;
+
+for (var i = 1; i = number; i++) {
+    if (i % 10 === 0) {
+        console.log(faktor)
+    }
+}
+
+//soal 2
+var input = 5;
+var temp = "";
+var str = "x";
+
+for (var i = 0; i < input; i++) {
+    for (var j = 0; j <= i; j++) {
+        if(i % 2 === 0){
+            temp = temp + str.toUpperCase        }
+            else {
+                temp = temp += "#"
+            }
+    }
+    console.log(temp)
+    temp = ""
+}
+
+//soal 3 (?)
+var input = 5;
+var temp = "";
+
+for (var i = 0; i < input; i++) {
+    for (var j = 0; j <= i; j++) {
+        temp += "#";
+    }
+    console.log(temp)
+    temp = ""
+}
