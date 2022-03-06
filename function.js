7df93e78cfed1e58c17f6731e61e773736ab543b
ngengngeng <oscarfabiorico76@gmail.com>
2022-03-06 22:40:07 +0700

    jawabanpertemuan1

---
+++ function.js
@@ -0,0 +1,50 @@
+//soal 1
+//algoritma
+// 1. buat function printNumbers
+// 2. buat variabel input
+// 3. isi variabel input dengan bilangan prima
+
+// pseudocode
+// 1. store function printNumbers(n)
+// 2. store variabel input
+// 3. set function printNumbers dengan bilangan prima
+
+
+
+// function printNumbers(n){
+//     for(var i = 1; i <= n; i++){
+//            if(i % 1 === 0){
+//                i === 2;
+//             }
+// }
+//             console.log(i)
+// }
+
+// printNumbers(5)
+
+//soal 2
+
+function phytagoras(a,b,c){
+   var p1 = a*a + b*b;
+   var p2 = c*c;
+   var a = Number;
+   var b = Number;
+   var c = Number;
+   var hasil = p1===p2;
+
+   for(var i = 1; i <= hasil; i++){
+       hasil += i
+   }
+
+   return hasil;
+}
+
+function triple(a,b,c){
+    if(hasil){
+        return "true"
+    }
+    else{
+        return "false"
+    }
+    }
+console.log(triple(3,4,5))
---
+++ variable.js
@@ -0,0 +1,90 @@
+// Soal 1
+/**
+ * ALGORITMA
+ * ------------
+ *
+ * 1. Buat variabel massa
+ * 2. Buat variabel volume
+ * 3. Buat variabel rho
+ * 4. Isi rho dengan rumus = massa bagi volume
+ * 5. Cetak rho
+ *
+ * PSEUDOCODE
+ * ----------
+ *
+ * STORE massa WITH ANY NUMBER
+ * STORE volume WITH ANY NUMBER
+
+ * SET rho WITH massa DIVIDED volume
+ * DISPLAY rho
+ */
+
+
+// Soal 2
+/**
+ * ALGORITMA
+ * ---------
+ *
+ * 1. Buat variabel tinggi
+ * 2. Buat variabel r
+ * 3. Buat variabel phi
+ * 4. Isi phi dengan 3.14
+ * 5. Buat variabel luas permukaan tabung
+ * 6. Isi luas permukaan tabung dengan rumus = 2 kali phi kali tinggi kali r tambah 2 kali phi kali r kali r
+ * 7. Cetak luas permukaan tabung
+ *
+ * PSEUDOCODE
+ * -----------
+ *
+ * STORE tinggi WITH ANY NUMBER
+ * STORE r WITH ANY NUMBER
+ * STORE phi WITH 3.14
+ 
+ * SET luas_permukaan_tabung WITH (2 * phi * tinggi * r) + (2 * phi * r * r)
+ * DISPLAY luas_permukaan_tabung
+ */
+
+/**
+ * Soal 3 
+ * ALGORITMA
+ * ----------
+ * 
+ * 1. Buat variabel r
+ * 2. Buat variabel phi
+ * 3. Isi phi dengan 3.14
+ * 4. Buat variabel volume
+ * 5. Isi volume dengan rumus 1/2 kali phi kali r kali r kali r
+ * 6. Cetak volume
+ * 
+ * PSEUDOCODE
+ * -----------
+ * 
+ * STORE r WITH ANY NUMBER
+ * STORE phi WITH 3.14
+
+ * SET volume WITH 1/2 MULTIPLY phi MULTIPLY r MULTIPLY r MULTIPLY r
+ * DISPLAY volume
+ */
+
+// CODING
+// var r =7;
+// var phi =3.14;
+
+// var volume = 1/2 * phi * r * r * r;
+// console.log(volume);
+
+// //soal 4
+
+//  var name = "Selamat Belajar, Rico!";
+//  console.log(name);
+
+//  //soal 5
+
+// var name = "admin";
+// var status = true;
+// var number = 1;
+// var result1 = ("admin" + number + status);
+// var number = 5;
+// var result2 = ("admin" + number + status);
+// console.log(result1);
+// console.log(result2);
