d0d5f812c12eedd659f96e4939bb7e90c578c8b4
ngengngeng <oscarfabiorico76@gmail.com>
2022-03-07 01:38:23 +0700

    jawabansoallooping

--- looping.js
+++ looping.js
@@ -1,37 +1,50 @@
 //soal 1
-var number = 10;
-var faktor = 1;
+// var number = 10;
+// var temp = "";
 
-for (var i = 1; i = number; i++) {
-    if (i % 10 === 0) {
-        console.log(faktor)
-    }
-}
+// for (var i = 1; i <= number; i++) {
+//     if (number % i === 0) {
+//         temp += i + ","
+//         console.log(temp)
+//     }
+// }
 
 // //soal 2
 // var input = 5;
 // var temp = "";
-// var str = "x";
 
 // for (var i = 0; i < input; i++) {
-//     for (var j = 0; j <= i; j++) {
-//         if(i % 2 === 0){
-//             temp = temp + str.toUpperCase        }
-//             else {
-//                 temp = temp += "#"
+//         for (var j = 0; j <= i; j++) {
+//             if(j % 2 === 0){
+//                 temp += "#";
 //             }
+//             else{
+//                 temp += "X";
+//             }
+//         }
+//         console.log(temp)
+//         temp = ""
 //     }
-//     console.log(temp)
-//     temp = ""
-// }
+
 
 // //soal 3 (?)
 // var input = 5;
 // var temp = "";
 
 // for (var i = 0; i < input; i++) {
-//     for (var j = 0; j <= i; j++) {
-//         temp += "#";
+//     for (var j = 0; j < input; j++) {
+//         if(i === 0){
+//             temp += "#"
+//         }
+//         else if(j % 5 === 0){
+//             temp += "#"
+//         }
+//         else if(i === 4){
+//             temp += "#"
+//         }
+//         else if(j % 4 === 0){
+//             temp += " " + " " + " " + "#"
+//         }
 //     }
 //     console.log(temp)
 //     temp = ""
